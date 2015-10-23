2ddca67600b2bcf4a5b2c61ce6fe0d44a25828a4
Jason Williams <jase.williams@gmail.com>
2015-10-23 17:11:15 +0100

    added volume knob

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,16 +7,18 @@ require(['oscillator'], function(Oscillator) {
     gainNode.connect(audioCtx.destination);
 
     /* Create a new oscillator which is a sine and at 500 hertz */
-    var osc1 = new Oscillator('sine', 329, 1, 0);
+    var osc1 = new Oscillator('sine', 210, 1, 0);
     osc1.start(1);
     osc1.stop(10);
     window.osc1 = osc1;
 
+    $('.dial').knob({fgColor: '#9C4059', bgColor: '#38C8EC', width: '50', height: '50', change:function(data) {  osc1.gainNode.gain.value = (data / 100) }});
+
     /* UI */
-    var centsControl = document.querySelector('#cents');
+    window.centsControl = document.querySelector('#cents');
     centsControl.addEventListener('change', function(data) {
+        console.log(this.value);
         osc1.setCents(this.value);
     });
 
-    $('.dial').knob({fgColor: "#9C4059", bgColor: "#38C8EC", width: "50"});
 });
